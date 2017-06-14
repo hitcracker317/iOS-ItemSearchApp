3eefa3e3bb0115b0342450367287c449745358de
hitcrakcer317 <dainaso97@gmail.com>
2017-06-14 16:31:38 +0900

    webAPIにリクエストを送信してレスポンスを受け取る

--- ItemSearchApp/data/ItemDataManager.swift
+++ ItemSearchApp/data/ItemDataManager.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+import Alamofire
+import SwiftyJSON
+
 class ItemDataManager {
     static let sharedInstance = ItemDataManager()
     
@@ -18,25 +21,14 @@ class ItemDataManager {
     
     func getItemData(text:String) -> [ItemData]{
         //入力したテキストを元にAPIに通信。その結果を配列に格納して返す
-        print("受け取った文字列は：\(text)")
-        
-        //TODO：
-        
-        
-        //そのURLを使用してWebAPIにリクエストを送る
-        //リクエストして返ってきたJSONをパースする
-        //パースした内容をItemDataのプロパティに代入して、ItemDataオブジェクトを生成
-        //生成したItemDataオブジェクトを配列に格納してviewControllerに渡す。
-        
-        
-        
         
         //appIDと検索文字の2点をエンコード
         let apiParameter = [
             "appid": appID,
             "query": text
         ]
-        var requstURL = getRequestURL(parameter: apiParameter)
+        let requstURL = getRequestURL(parameter: apiParameter)
+        getData(requestURL: requstURL)
         
         
         return ItemDataArray
@@ -69,5 +61,38 @@ class ItemDataManager {
         
         let requestURL = entryUrl + parameterArray[0] + "&" + parameterArray[1]
         return requestURL
+        
+        
+    }
+    
+    //WebAPIにリクエストを送信
+    func getData(requestURL: String) {
+        Alamofire.request(requestURL, method: .get).responseJSON{ response in
+            
+            guard let itemData = response.result.value else{
+                print("json取得できねえ！")
+                return
+            }
+            
+            let jsonData = JSON(itemData)["ResultSet"]["0"]["Result"] //取得した商品データ一覧のJSON
+            print("jsonの中身：\(jsonData)")
+            
+            for (index,_):(String, JSON) in jsonData {
+                //JSONパースして商品情報のオブジェクトを作成する
+                self.createItemData(jsonData: jsonData, index: index)
+            }
+            
+        }
+    }
+    
+    func createItemData(jsonData: JSON, index: String) {
+        let itemData = ItemData()
+        
+        itemData.imageImageUrl = jsonData["\(index)"]["Image"]["Medium"].string
+        itemData.itemTitle = jsonData["\(index)"]["Nmae"].string
+        itemData.itemPrice = jsonData["\(index)"]["Price"]["_value"].string
+        itemData.itemUrl = jsonData["\(index)"]["Url"].string
+        
+        ItemDataArray.append(itemData)
     }
 }
---
+++ Pods/Target Support Files/Pods-ItemSearchApp/Pods-ItemSearchApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ItemSearchApp : NSObject
+@end
+@implementation PodsDummy_Pods_ItemSearchApp
+@end
